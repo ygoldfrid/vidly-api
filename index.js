aee85e6fad71f5613a997569bbf1df221e8a773d
ygoldfrid <ygoldfrid@gmail.com>
2020-06-07 10:00:47 -0500

    Added Cors Support

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const winston = require("winston");
 const express = require("express");
+const config = require("config");
 const app = express();
 
 require("./startup/logging")();
+require("./startup/prod")(app);
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
-require("./startup/prod")(app);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || config.get("port");
 const server = app.listen(port, () =>
   winston.info(`Listening on port ${port}...`)
 );
